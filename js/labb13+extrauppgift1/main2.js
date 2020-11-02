bcf44e806014d8d201ab71412d21d5f57c4c5d9e
Elin Ståbi <elinstabi@Elins-MacBook.local>
2020-11-02 08:47:25 +0100

    2.0 with dates

--- js/labb13+extrauppgift1/main2.js
+++ js/labb13+extrauppgift1/main2.js
@@ -1,5 +1,6 @@
-//labb13 och extrauppgift 1 // labb/upppgiftförklaring längst ner
-
+// Labb13 och extrauppgift 1 
+// Förbättringar, datum ej kunna klicka i dag om dagen ej finns på månaden
+// samt kunna ta bort en student som ändå är sparad vid tidigare datum
 
 // Validating the inputs when leaving them
 const inputs = document.querySelectorAll('form input');
@@ -39,13 +40,13 @@ const resetForm = form => {
 
 // Class for creating new class objetcs
 class Student {
-	constructor(name, email, tel, course, date, id) {
+	constructor(name, email, tel, course, id) {
 		this.name = name;
 		this.email = email;
 		this.tel = tel;
 		this.course = course;
-		this.date = date;
 		this.id = id;
+		this.presence = {};
 	}
 	studentlisthtml() {
 		return `<tr><td><div class="input-group d-flex flex-column text-nowrap"><div><input id="present${student.id}" role="button" type="radio" name="attendance${student.id}"><label class="ml-2" role="button" for="present${student.id}">Närvarande</label></div><div><input id="shortAbsence${student.id}" role="button" type="radio" name="attendance${student.id}"><label class="ml-2" role="button" for="shortAbsence${student.id}">Kortare frånvaro</label></div><div><input id="absence${student.id}" role="button" type="radio" name="attendance${student.id}"><label class="ml-2" role="button" for="absence${student.id}">Frånvaro</label></div><div><input id="ill${student.id}" role="button" type="radio" name="attendance${student.id}"><label class="ml-2" role="button" for="ill${student.id}">Sjuk</label></div></div></td>
@@ -53,18 +54,12 @@ class Student {
 					<td><button class="btn btn-secondary btn-sm text-nowrap py-0" data-removestudent="${student.id}" type="button">Ta bort</button></td>
 				</tr>`;
 	}
-}
-
-// Class extend for when adding precence //this can also be directly in Student but wanted to try the extends
-class Presence extends Student {
-	constructor(name, email, tel, course, date, id, presence) {
-	  super(name, email, tel, course, date, id);
-	  this.presence = presence;
-	}
-	addpresency() {
-		const presencies = document.querySelector(`#${student.presence}`);
-		if (presencies) {
-			presencies.setAttribute('checked', 'checked');
+	addpresency(stamp) {
+		for(let studentinlist of studentlist){
+			const presencies = document.querySelector(`#${studentinlist.presence[stamp]}`);
+			if (presencies) {
+				presencies.checked = true;
+			}
 		}
 	}
 }
@@ -75,15 +70,13 @@ let studentlist = [];
 // Adding student to list function
 const addStudent = () => {
 	const selectedCourse = document.querySelector('[data-studentselect="edu"]').value;
-	const date = new Date();
 	const id = Date.now();
 
 	student = new Student(
-		document.querySelector('#studentName').value,
-		document.querySelector('#studentEmail').value,
-		document.querySelector('#studentTel').value,
+		document.querySelector('[data-studentinput="name"]').value,
+		document.querySelector('[data-studentinput="email"]').value,
+		document.querySelector('[data-studentinput="tel"]').value,
 		document.querySelector('[data-studentselect="edu"]').value,
-		date,
 		id,
 	);
 	
@@ -103,7 +96,8 @@ const radioBtnOnChange = () => {
 				const regex = /\d+/g;
 				const addPresency = e.target.id.match(regex);
 				if (parseInt(addPresency) === studentinlist.id){
-					studentinlist.presence = e.target.id;
+					const stamp = `${document.querySelector('[data-date="getMonth"]').value}:${document.querySelector('[data-date="getDay"]').value}:${document.querySelector('[data-date="getMinutes"]').value}`;
+					studentinlist.presence[stamp] = `${e.target.id}`;
 					saveToLocalStorage();
 				}
 			}
@@ -112,6 +106,7 @@ const radioBtnOnChange = () => {
 }
 
 // Removes student in list in studenlistarray and localstorage
+// function 2.0 would be to keep student the previous days
 const removeStudentOnClick = () => {
 	const removeStudentBtn = document.querySelectorAll('[data-removestudent]');
 	removeStudentBtn.forEach(removeBtn => {
@@ -123,24 +118,58 @@ const removeStudentOnClick = () => {
 	})
 }
 
+const getDateSelects = document.querySelectorAll('[data-date]');
+// When changing dateselect get aqurate presency
+const dateSelectOnChange = () => {
+	getDateSelects.forEach(select => {
+		select.addEventListener('change', () => {
+			seletedDate();
+		})
+	})
+	const seletedDate = () => {
+		// Removes checked on checkboxes
+		const checkedRadioBtn = document.querySelectorAll('[type="radio"]');
+		checkedRadioBtn.forEach(radioBtn => {
+			if (radioBtn.checked === true) {
+				radioBtn.checked = false;
+			}
+		})
+		// Sets new radio checked based on studentlist at the specific date
+		const stamp = `${document.querySelector('[data-date="getMonth"]').value}:${document.querySelector('[data-date="getDay"]').value}:${document.querySelector('[data-date="getMinutes"]').value}`;
+		student.addpresency(stamp);
+	}
+}
+
+// Set date on selects on load. Choosing not to save to localstorage so not by misstake changing student
+// precency on wrong date. Also with min for testing function not needing to wait one day to see if works.
+const setDate = () => {
+	const getDate = new Date();	
+	for(select of getDateSelects) {
+		const getDatasetGetdate = select.dataset.date;
+		const convertDatasetToMethod = getDate[`${getDatasetGetdate}`]();
+		select.value = convertDatasetToMethod;
+	}
+}
+
 // Displaying the students from studentlistarray on init
 const displayStudents = () => {
 	for(let studentinlist of studentlist){
-		student = new Presence(
+		student = new Student(
 			studentinlist.name,
 			studentinlist.email,
 			studentinlist.tel,
 			studentinlist.course,
-			studentinlist.date,
 			studentinlist.id,
 			studentinlist.presence,
 		);
 		document.querySelector(`[data-studentlist="${studentinlist.course}"]`).innerHTML += student.studentlisthtml();
-		student.addpresency();
+		const stamp = `${document.querySelector('[data-date="getMonth"]').value}:${document.querySelector('[data-date="getDay"]').value}:${document.querySelector('[data-date="getMinutes"]').value}`;
+		student.addpresency(stamp);
 	}
 }
 
 // Saving the studentlist to localstorage
+// localStorage.clear(); // if wanting to clear localstorage
 const saveToLocalStorage = () => {
 	localStorage.setItem('studenlists', JSON.stringify(studentlist));
 }
@@ -152,9 +181,11 @@ const init = () => {
 	if(!studentlist){
 		studentlist = [];
 	}
+	setDate();
 	displayStudents();
 	radioBtnOnChange();
 	removeStudentOnClick();
+	dateSelectOnChange();
 }
 
 init();
