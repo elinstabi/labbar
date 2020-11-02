565a34a07a5e838c523c0adc044acc045ffee56f
Elin Ståbi <elinstabi@Elins-MacBook.local>
2020-11-02 08:44:37 +0100

    1.5 with extend no dates

--- js/labb13+extrauppgift1/main2.js
+++ js/labb13+extrauppgift1/main2.js
@@ -1,15 +1,8 @@
 //labb13 och extrauppgift 1 // labb/upppgiftförklaring längst ner
-/*
-Jag ändrade lite i din kod för att visa ett litet exempel på hur du kan använda dig av en 
-Student-klass samt spara data till localstorage, jag fixade bara funktionalitet för att 
-lägga till samt ta bort studenter, men jag tänkte att för närvaro så kan man koppla 
-radioknapparna till en onchange Eventlistener och när det triggas så kan man ändra 
-närvaro i Student-objektet i dess precense property e,g precense: { "2020_20_28": "Sick" }.
-Jag bifogar filen med exemplet samt en screenshot som visar ett exempel hur data är lagrad i localstorage.
-*/
-//localStorage.clear();
-// validating the inputs when leaving them
-const inputs = document.querySelectorAll('input');
+
+
+// Validating the inputs when leaving them
+const inputs = document.querySelectorAll('form input');
 inputs.forEach(input => {
 	input.addEventListener('blur', () => {
 		input.classList.remove('is-valid', 'is-invalid');
@@ -17,7 +10,7 @@ inputs.forEach(input => {
 	})
 })
 
-// validating the form on submit
+// Validating the form on submit
 const form = document.querySelector('.needs-validation');
 form.addEventListener('submit', e => {
 	if (form.checkValidity() === false) {
@@ -30,7 +23,7 @@ form.addEventListener('submit', e => {
 	e.stopPropagation();
 })
 
-//reseting form after each submit
+// Reseting form after each submit
 const resetForm = form => {
 	form.reset();
 	form.classList.remove('was-validated');
@@ -44,7 +37,7 @@ const resetForm = form => {
 	}, 4000);
 }
 
-// class for creating new class objetcs
+// Class for creating new class objetcs
 class Student {
 	constructor(name, email, tel, course, date, id) {
 		this.name = name;
@@ -54,28 +47,26 @@ class Student {
 		this.date = date;
 		this.id = id;
 	}
-	present() {
+	studentlisthtml() {
 		return `<tr><td><div class="input-group d-flex flex-column text-nowrap"><div><input id="present${student.id}" role="button" type="radio" name="attendance${student.id}"><label class="ml-2" role="button" for="present${student.id}">Närvarande</label></div><div><input id="shortAbsence${student.id}" role="button" type="radio" name="attendance${student.id}"><label class="ml-2" role="button" for="shortAbsence${student.id}">Kortare frånvaro</label></div><div><input id="absence${student.id}" role="button" type="radio" name="attendance${student.id}"><label class="ml-2" role="button" for="absence${student.id}">Frånvaro</label></div><div><input id="ill${student.id}" role="button" type="radio" name="attendance${student.id}"><label class="ml-2" role="button" for="ill${student.id}">Sjuk</label></div></div></td>
 					<td>${student.name}</td><td>${student.email}</td><td>${student.tel}</td>	
-					<td><button class="btn btn-secondary btn-sm text-nowrap py-0" data-removestudent="true" type="button">Ta bort</button></td>
+					<td><button class="btn btn-secondary btn-sm text-nowrap py-0" data-removestudent="${student.id}" type="button">Ta bort</button></td>
 				</tr>`;
 	}
 }
 
-class Precence extends Student {
-	constructor(name, email, tel, course, date, id, precence) {
+// Class extend for when adding precence //this can also be directly in Student but wanted to try the extends
+class Presence extends Student {
+	constructor(name, email, tel, course, date, id, presence) {
 	  super(name, email, tel, course, date, id);
-	  this.precence = precence;
+	  this.presence = presence;
 	}
-	show() {
-		return this.present()
-	}
-	/*hej() {
-		if (document.querySelector(`#${student.precence}`)) {
-			console.log('hej');
-			document.querySelector(`#${student.precence}`).checked = true;
+	addpresency() {
+		const presencies = document.querySelector(`#${student.presence}`);
+		if (presencies) {
+			presencies.setAttribute('checked', 'checked');
 		}
-	}*/
+	}
 }
 
 // Array for added students
@@ -95,55 +86,63 @@ const addStudent = () => {
 		date,
 		id,
 	);
-
-	document.querySelector(`[data-studentlist="${selectedCourse}"]`).innerHTML += student.present();
 	
+	document.querySelector(`[data-studentlist="${selectedCourse}"]`).innerHTML += student.studentlisthtml();
 	studentlist.push(student);
-	
 	saveToLocalStorage();	
-	radioBtnSaveLocalStorageOnChange();
+	radioBtnOnChange();
+	removeStudentOnClick();
 }
 
-const radioBtnSaveLocalStorageOnChange = () => {
+// Adds presency to students on radiobutton change and saves it to localstorage
+const radioBtnOnChange = () => {
 	const checkedRadioBtn = document.querySelectorAll('[type="radio"]');
-	if (checkedRadioBtn) {
-		checkedRadioBtn.forEach(radioBtn => {
-			radioBtn.addEventListener('change', e => {
-				student = new Precence(
-					student.name,
-					student.email,
-					student.tel,
-					student.course,
-					student.date,
-					student.id,
-					e.currentTarget.id
-				);
-				studentlist = studentlist.filter(s => s.id !== student.id);
-				studentlist.push(student);
-				saveToLocalStorage();
-			})
+	checkedRadioBtn.forEach(radioBtn => {
+		radioBtn.addEventListener('change', e => {
+			for(let studentinlist of studentlist){
+				const regex = /\d+/g;
+				const addPresency = e.target.id.match(regex);
+				if (parseInt(addPresency) === studentinlist.id){
+					studentinlist.presence = e.target.id;
+					saveToLocalStorage();
+				}
+			}
 		})
-	}
+	})
 }
 
-
-const saveToLocalStorage = () => {
-	localStorage.setItem('studenlists', JSON.stringify(studentlist));
+// Removes student in list in studenlistarray and localstorage
+const removeStudentOnClick = () => {
+	const removeStudentBtn = document.querySelectorAll('[data-removestudent]');
+	removeStudentBtn.forEach(removeBtn => {
+		removeBtn.addEventListener('click', e => {
+			e.target.closest('tr').remove();
+			studentlist = studentlist.filter(s => s.id !== parseInt(removeBtn.dataset.removestudent));
+			saveToLocalStorage();
+		})
+	})
 }
 
-const displayStudentsFromLocalStorage = () => {
+// Displaying the students from studentlistarray on init
+const displayStudents = () => {
 	for(let studentinlist of studentlist){
-		student = new Precence(
+		student = new Presence(
 			studentinlist.name,
 			studentinlist.email,
 			studentinlist.tel,
 			studentinlist.course,
 			studentinlist.date,
 			studentinlist.id,
-			studentinlist.precence,
+			studentinlist.presence,
 		);
-		document.querySelector(`[data-studentlist="${studentinlist.course}"]`).innerHTML += student.show();
+		document.querySelector(`[data-studentlist="${studentinlist.course}"]`).innerHTML += student.studentlisthtml();
+		student.addpresency();
 	}
+}
+
+// Saving the studentlist to localstorage
+const saveToLocalStorage = () => {
+	localStorage.setItem('studenlists', JSON.stringify(studentlist));
 }
 
 const init = () => {
@@ -153,8 +152,9 @@ const init = () => {
 	if(!studentlist){
 		studentlist = [];
 	}
-	displayStudentsFromLocalStorage();
-	radioBtnSaveLocalStorageOnChange();
+	displayStudents();
+	radioBtnOnChange();
+	removeStudentOnClick();
 }
 
 init();
